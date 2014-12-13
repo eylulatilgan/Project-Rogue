9cf8cac7c83e3863e39899a8c2758f7b6879edb1
Ömür Can Güngören <orumcan@gmail.com>
2014-12-13 05:05:46 +0200

    Update RogueController.java
    Created a method for getting the map size from the user.

--- RogueExecutor.java
+++ RogueExecutor.java
@@ -16,7 +16,29 @@ public class RogueExecutor {
 	
 	public static void main(String[] args){
 		
+		gettingMapSize();
 		
+	}
+
+
+
+	private static void gettingMapSize() {
+		int inputSize = 0;
+		
+		Scanner sc = new Scanner(System.in);
+
+        while (inputSize < 10 || inputSize > 20) {
+            System.out.print("MAP SIZE (10-20): ");
+            while(!sc.hasNextInt()) {
+                System.out.print("TRY TO USE NUMBERS IF YOU CAN: ");
+                sc.next();
+            }
+            inputSize = sc.nextInt();
+            if (inputSize < 10 || inputSize > 20) {
+                System.out.println("'(10-20)' MEANS; YOU CAN ONLY USE NUMBERS BETWEEN 10 TO 20. SO PLEASE... ");
+            }
+        }
+        sc.close();
 	}
 	
 	
