7aaa83ef0b760cf46d51e836a92f0d8b2da6d23a
Ömür Can Güngören <orumcan@gmail.com>
2014-12-13 04:59:09 +0200

    add RogueExecutor
    main method has been created and initial variables has been added.

---
+++ RogueExecutor.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class RogueExecutor {
+
+	Object[][] map;
+	int boundarySize = 10;
+	int score = 0;
+	int level = 0;
+	int x;
+	int y;
+	boolean isGameOver = false;
+	String consoleTxt = "";
+	
+	
+	
+	public static void main(String[] args){
+		
+		
+	}
+	
+	
+}
