6e5abae623d3366b0e2f54173c501f8829c05300
codermoon <eylul.atilgan@gmail.com>
2014-12-15 22:09:22 +0200

    main menu implementation

--- MainMenu.java
+++ MainMenu.java
@@ -1,6 +1,19 @@
-import java.awt.Frame;
+import java.awt.*;
+import java.awt.font.FontRenderContext;
+import java.awt.geom.Rectangle2D;
 
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
-public class MainMenu extends Frame {
-	
+public class MainMenu extends JPanel {
+	JPanel    mainMenuPanel = new JPanel();
+	JTextArea nameOfTheGame = new JTextArea(10, 40);
+
+	 public static void main(String[] args) {
+	      JFrame f = new JFrame();
+	      f.getContentPane().add(new MainMenu());
+	      f.setSize(800, 600);
+	      f.setVisible(true);
+	   }
 }
