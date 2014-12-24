71e7220f3a07b2ed542200aeeac73be92cffac04
Ömür Can Güngören <orumcan@gmail.com>
2014-12-24 12:20:31 +0200

    Update RogueController.java
    Completed the RogueController class with the movement methods

--- RogueController.java
+++ RogueController.java
@@ -1,4 +1,38 @@
 
 public class RogueController {
 
+
+	Player player;
+	Map map;
+	int playerX = player.getX();
+	int playerY = player.getY();
+	int mapSize = map.mapSize;
+	
+	public RogueController(Player player, Map map){
+		this.player = player;
+		this.map = map;
+	}
+	public void moveNorth(){
+		
+		if(playerY > 0 && playerY < mapSize)
+			player.setY(playerY-1);
+	}
+	
+	public void moveSouth(){
+		
+		if(playerY > 0 && playerY < mapSize)
+			player.setY(playerY+1);
+	}
+	
+	public void moveEast(){
+
+		if(playerX > 0 && playerX < mapSize)
+			player.setX(playerX+1);
+	}
+	
+	public void moveWest(){
+
+		if(playerX > 0 && playerX < mapSize)
+			player.setX(playerX-1);
+	}
 }
