1983c79dd7b8b4da4c3f9d587fe6b64ccf45e69e
codermoon <eylul.atilgan@gmail.com>
2014-12-28 23:10:02 +0200

    Initializing Map

--- DummyRogueExecutor.java
+++ DummyRogueExecutor.java
@@ -2,7 +2,6 @@ import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Random;
-import java.util.Scanner;
 
 import javax.swing.JButton;
 import javax.swing.JDialog;
@@ -12,7 +11,7 @@ import javax.swing.JTextField;
 
 public class DummyRogueExecutor {
 
-	static Map map;
+	private Map map;
 	static int boundarySize = 10;
 	int x;
 	int y;
@@ -22,11 +21,10 @@ public class DummyRogueExecutor {
 	/*private JFrame dialogFrame;
 	private JDialog dialog;*/
 	//private JTextField boundarySizeInput;
-	private int trueBoundarySize;
+	private int dummyBoundarySize;
 
 	
 	/*public static void execute(){
-		getMapSize();
 		callMapGenerator();
 		if(isGameOver)
 			System.exit(0);
@@ -36,7 +34,7 @@ public class DummyRogueExecutor {
 		getMapSize();
 	}
 	
-	 public static void main(String args[])
+	public static void main(String args[])
 	    {
 	        new DummyRogueExecutor();
 	    }
@@ -62,8 +60,8 @@ public class DummyRogueExecutor {
 			public void actionPerformed(ActionEvent e) {
 				int size = Integer.parseInt(boundarySizeInput.getText());
 				if(size <= 20 && 10 <= size){
-					trueBoundarySize = size;
-				 	System.out.println(trueBoundarySize);
+					dummyBoundarySize = size;
+					callMapGenerator();
 				}
 				else
 					dialog.add(new JLabel(" (10-20) MEANS; YOU CAN ONLY USE NUMBERS BETWEEN 10 TO 20. SO PLEASE... "));
@@ -74,32 +72,177 @@ public class DummyRogueExecutor {
         
         dialog.setLocationRelativeTo(null);
         dialog.setVisible(true);
-		
-		
-		/*Scanner sc = new Scanner(System.in);
-        while (boundarySize < 10 || boundarySize > 20) {
-        	consoleTxt += "\nMAP SIZE (10-20): ";
-            while(!sc.hasNextInt()) {
-            	consoleTxt += "\nTRY TO USE NUMBERS IF YOU CAN: ";
-                sc.next();
-            }
-            boundarySize = sc.nextInt();
-            if (boundarySize < 10 || boundarySize > 20) {
-            	consoleTxt += "\n'(10-20)' MEANS; YOU CAN ONLY USE NUMBERS BETWEEN 10 TO 20. SO PLEASE... ";
-            }
-        }
-        sc.close();
-        consoleTxt = "START!";*/
 	}
 	
-	
-	
-	public static void callMapGenerator(){
-		map = new Map();
-		map.generateMap();
+	public void callMapGenerator(){
+		map = new Map(dummyBoundarySize);
+		System.out.println(map.getMapBoundary());
+		initUI(map);
 	}
 
+	
+	 private void initUI(Map map) {
+		 for(int i = 0; i < map.getMapBoundary(); i++){
+			 for(int j = 0; j < map.getMapBoundary(); j++){
+				 System.out.println(map.getMapArray()[i][j].getRoomContent());
+			 }
+		 }	
+	}
 }
+
+class Room {
+	
+	private boolean isExplored;
+	private String roomContent = ""; //T -> trap, E -> enemy, S -> sword, G -> gold, 0 -> empty
+	
+	public boolean getIsExplored(){
+		return isExplored;
+	}
+	
+	public void setExplored(boolean isExplored){
+		this.isExplored = isExplored;
+	}
+	
+	public void setRoomContent(String roomContent){
+		this.roomContent = roomContent;
+	}
+	
+	public String getRoomContent(){
+		return roomContent;
+	}
+	
+	
+}
+
+class Map {
+	
+	private int mapSize;
+	protected Room[][] mapArray;
+	
+	public Map(int mapSize){
+		this.mapSize = mapSize;
+		mapArray = new Room[mapSize][mapSize];
+		generateMap();
+	}
+	
+	public void generateMap(){
+		//System.out.println("hello I'm here!! ");
+		initRooms();
+		//placeTraps();
+		//placeEnemies();
+		//placeGold();
+		//placeSword();
+	}
+	
+	public void initRooms(){
+		System.out.println("hello I'm here!! ");
+		for (int i = 0; i < mapSize; i++){
+			for(int j = 0; j < mapSize; j++){
+				mapArray[i][j] = new Room();
+				mapArray[i][j].setExplored(false);
+				mapArray[i][j].setRoomContent("0");
+			}
+		}
+	}
+	
+	public Room[][] getMapArray(){
+		return mapArray;
+	}
+	
+	public int getMapBoundary(){
+		return mapSize;
+	}
+	
+//	public void placeTraps(){ //assumes that the dungeon has "mapSize" amount of traps.
+//		Random random = new Random();
+//		int trapCount = 0;
+//		while(trapCount < mapSize){
+//			int x = random.nextInt(mapSize);
+//			int y = random.nextInt(mapSize);
+//			if(mapArray[x][y].roomContent.equals("0")){ //if there is nothing in the room
+//				mapArray[x][y].roomContent = "E";
+//			} else {
+//				continue;
+//			}
+//			trapCount++;
+//		}
+//	}
+//	
+//	public void placeEnemies(){ //assumes that the dungeon has "mapSize" amount of enemies.
+//		Random random = new Random();
+//		int enemiesCount = 0;
+//		while(enemiesCount < mapSize){
+//			int x = random.nextInt(mapSize);
+//			int y = random.nextInt(mapSize);
+//			if(mapArray[x][y].roomContent.equals("0")){ //if there is nothing in the room
+//				mapArray[x][y].roomContent = "E";
+//			} else {
+//				continue;
+//			}
+//			enemiesCount++;
+//		}
+//	}
+//	
+//	public void placeGold(){ //assumes that the dungeon has "mapSize" amount of traps.
+//		Random random = new Random();
+//		int goldCount = 0;
+//		while(goldCount < mapSize){
+//			int x = random.nextInt(mapSize);
+//			int y = random.nextInt(mapSize);
+//			if(mapArray[x][y].roomContent.equals("0")){ //if there is nothing in the room
+//				mapArray[x][y].roomContent = "G";
+//			} else {
+//				continue;
+//			}
+//			goldCount++;
+//		}
+//	}
+//	
+//	public void placeSword(){ //places only one sword.
+//		Random random = new Random();
+//			int x = random.nextInt(mapSize);
+//			int y = random.nextInt(mapSize);
+//			if(mapArray[x][y].roomContent.equals("0")){ //if there is nothing in the room
+//				mapArray[x][y].roomContent = "S";
+//			}
+//	}
+//	
+//	public void checkRoom(){
+//		int x = Player.x;
+//		int y = Player.y;
+//		if(mapArray[x][y].roomContent.equals("T")){
+//			DummyRogueExecutor.consoleTxt += "\nYOU ACTIVATED A TRAP AND DIED!\nGAME OVER!";
+//			DummyRogueExecutor.isGameOver = true;
+//		}
+//		if(mapArray[x][y].roomContent.equals("E")){	
+//			DummyRogueExecutor.consoleTxt += "\nYOU HAVE ENCOUNTERED AN ENEMY! GET READY FOR BATTLE!";
+//			if(DummyRogueExecutor.hasInteracted){
+//				if(Player.level >= Enemy.level){
+//					DummyRogueExecutor.consoleTxt += "\nYOU WON .........";
+//					Player.score += 10 * Enemy.level;
+//				} else {
+//					DummyRogueExecutor.consoleTxt += "\nYOU LOST, DIED .........";
+//					DummyRogueExecutor.isGameOver = true;
+//				}
+//			}
+//		}
+//		if(mapArray[x][y].roomContent.equals("S")){	
+//			if(Player.level<4){
+//				DummyRogueExecutor.consoleTxt += "\nYOU HAVE FOUND A SWORD. NOW YOU CAN FACE THE TOUGHER MONSTERS!";
+//				Player.level++;
+//			} else {
+//				DummyRogueExecutor.consoleTxt += "\nYOU HAVE FOUND SOME GOLD! 'SHINY!'";
+//				Player.score += 10;
+//			}
+//		}
+//		if(mapArray[x][y].roomContent.equals("G")){	
+//			DummyRogueExecutor.consoleTxt += "\nYOU HAVE FOUND SOME GOLD! 'SHINY!'";
+//			Player.score += 10;
+//		}
+//	}
+	
+}
+
 
 class Player {
 	
@@ -187,134 +330,6 @@ class Enemy {
 	public void setY(int y) {
 		this.y = y;
 	}	
-	
-}
-
-class Room {
-	
-	boolean isExplored;
-	String roomContent = ""; //T -> trap, E -> enemy, S -> sword, G -> gold, 0 -> empty
-	
-	public boolean getIsExplored(){
-		return isExplored;
-	}
-	
-}
-
-class Map {
-	
-	int mapSize;
-	Room[][] mapArray;
-	
-	public Map(){
-		mapSize = DummyRogueExecutor.boundarySize;
-		mapArray = new Room[mapSize][mapSize];
-	}
-	
-	public void generateMap(){
-		initRooms();
-		placeTraps();
-		placeEnemies();
-		placeGold();
-		placeSword();
-	}
-	
-	public void initRooms(){
-		for (int i = 0; i<mapSize; i++){
-			for(int j = 0; j<mapSize; j++){
-				mapArray[i][j].isExplored = false;
-				mapArray[i][j].roomContent = "0";
-			}
-		}
-	}
-		
-	public void placeTraps(){ //assumes that the dungeon has "mapSize" amount of traps.
-		Random random = new Random();
-		int trapCount = 0;
-		while(trapCount < mapSize){
-			int x = random.nextInt(mapSize);
-			int y = random.nextInt(mapSize);
-			if(mapArray[x][y].roomContent.equals("0")){ //if there is nothing in the room
-				mapArray[x][y].roomContent = "E";
-			} else {
-				continue;
-			}
-			trapCount++;
-		}
-	}
-	
-	public void placeEnemies(){ //assumes that the dungeon has "mapSize" amount of enemies.
-		Random random = new Random();
-		int enemiesCount = 0;
-		while(enemiesCount < mapSize){
-			int x = random.nextInt(mapSize);
-			int y = random.nextInt(mapSize);
-			if(mapArray[x][y].roomContent.equals("0")){ //if there is nothing in the room
-				mapArray[x][y].roomContent = "E";
-			} else {
-				continue;
-			}
-			enemiesCount++;
-		}
-	}
-	
-	public void placeGold(){ //assumes that the dungeon has "mapSize" amount of traps.
-		Random random = new Random();
-		int goldCount = 0;
-		while(goldCount < mapSize){
-			int x = random.nextInt(mapSize);
-			int y = random.nextInt(mapSize);
-			if(mapArray[x][y].roomContent.equals("0")){ //if there is nothing in the room
-				mapArray[x][y].roomContent = "G";
-			} else {
-				continue;
-			}
-			goldCount++;
-		}
-	}
-	
-	public void placeSword(){ //places only one sword.
-		Random random = new Random();
-			int x = random.nextInt(mapSize);
-			int y = random.nextInt(mapSize);
-			if(mapArray[x][y].roomContent.equals("0")){ //if there is nothing in the room
-				mapArray[x][y].roomContent = "S";
-			}
-	}
-	
-	public void checkRoom(){
-		int x = Player.x;
-		int y = Player.y;
-		if(mapArray[x][y].roomContent.equals("T")){
-			DummyRogueExecutor.consoleTxt += "\nYOU ACTIVATED A TRAP AND DIED!\nGAME OVER!";
-			DummyRogueExecutor.isGameOver = true;
-		}
-		if(mapArray[x][y].roomContent.equals("E")){	
-			DummyRogueExecutor.consoleTxt += "\nYOU HAVE ENCOUNTERED AN ENEMY! GET READY FOR BATTLE!";
-			if(DummyRogueExecutor.hasInteracted){
-				if(Player.level >= Enemy.level){
-					DummyRogueExecutor.consoleTxt += "\nYOU WON .........";
-					Player.score += 10 * Enemy.level;
-				} else {
-					DummyRogueExecutor.consoleTxt += "\nYOU LOST, DIED .........";
-					DummyRogueExecutor.isGameOver = true;
-				}
-			}
-		}
-		if(mapArray[x][y].roomContent.equals("S")){	
-			if(Player.level<4){
-				DummyRogueExecutor.consoleTxt += "\nYOU HAVE FOUND A SWORD. NOW YOU CAN FACE THE TOUGHER MONSTERS!";
-				Player.level++;
-			} else {
-				DummyRogueExecutor.consoleTxt += "\nYOU HAVE FOUND SOME GOLD! 'SHINY!'";
-				Player.score += 10;
-			}
-		}
-		if(mapArray[x][y].roomContent.equals("G")){	
-			DummyRogueExecutor.consoleTxt += "\nYOU HAVE FOUND SOME GOLD! 'SHINY!'";
-			Player.score += 10;
-		}
-	}
 	
 }
 
