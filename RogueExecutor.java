ab296984d1799d4f42dd805f3298218e4531421d
Ömür Can Güngören <orumcan@gmail.com>
2014-12-30 13:28:06 +0200

    Update RogueExecutor.java
    map and control update

--- RogueExecutor.java
+++ RogueExecutor.java
@@ -1,4 +1,3 @@
-import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.EventQueue;
 import java.awt.FlowLayout;
@@ -22,65 +21,70 @@ import javax.swing.JTextField;
 import javax.swing.border.Border;
 
 public class RogueExecutor {
+
+
+	static int boundarySize = 10;
+	int x;
+	int y;
+
 	static int startX;
 	static int startY;
+	static int trapX;
+	static int trapY;
 	static boolean isGameOver = false;
-	static boolean hasInteracted = false;
 	static String consoleTxt = "";
-	
-	private static final int CELL_SIZE = 30;
-	private static final int CONSOLE_PANEL_SIZE = 200;
-	private int boundarySize;
+	static boolean hasInteracted = false;
+	JLabel consoleTxtLabel;
+	/*private JFrame dialogFrame;
+	private JDialog dialog;*/
+	//private JTextField boundarySizeInput;
+	private int BoundarySize;
 	private JFrame gameFrame;
 	private JPanel gamePanel;
 	private Map map;
 	private Player player;
+	private Enemy enemy;
 	private JLabel [][] cells;
 	private RogueController controller;
-	private JPanel consolePanel;
+	private static final int CELL_SIZE = 30;
 	private ImageIcon characterIcon, roomIcon, emptyRoomIcon, enemyIcon, goldIcon, trapIcon, swordPNG; 
-	private JLabel character, room, emptyRoom, enemy, gold, trap, sword;
-	
+	private JLabel character, room, emptyRoom, enemyRoom, gold, trap, sword;
+	private static final int CONSOLE_PANEL_SIZE = 200;
+	private JPanel consolePanel;
+
 	public RogueExecutor(){
 		characterIcon = new ImageIcon("ico-x.png");
 		getMapSize();
 	}
-	
-	public static void main(String args[])
-	    {
-	       Runnable r  = new Runnable() {
-	            @Override
-	            public void run() {
-	            	new RogueExecutor();
-	            }
-	        };
 
-	        EventQueue.invokeLater(r);
-	    }
+	public static void main(String args[])
+	{
+		new RogueExecutor();
+	}
 
 	private void getMapSize() {
-		
+
 		JFrame dialogFrame = new JFrame("Rogue Game");
 		final JTextField boundarySizeInput = new JTextField(20); // final may not be needed due to an error
 		JButton OKbutton = new JButton("OK!");
-	
+
 		dialogFrame.setLayout(new FlowLayout());
 		JDialog.setDefaultLookAndFeelDecorated(true);
-		
+
 		final JDialog dialog = new JDialog(dialogFrame, "ROGUE GAME"); // final may not be needed due to an error
 		dialog.setSize(500, 100);
 		dialog.setLayout(new FlowLayout());
-        
-        dialog.add(new JLabel("MAP SIZE (10-20): "));
-        dialog.add(boundarySizeInput);
-        dialog.add(OKbutton);
 
-        OKbutton.addActionListener(new ActionListener(){
+		dialog.add(new JLabel("MAP SIZE (10-20): "));
+		dialog.add(boundarySizeInput);
+		dialog.add(OKbutton);
+
+		OKbutton.addActionListener(new ActionListener(){
 			public void actionPerformed(ActionEvent e) {
 				int size = Integer.parseInt(boundarySizeInput.getText());
 				try{
 					if(size <= 20 && 10 <= size){
-						boundarySize = size;
+						BoundarySize = size;
 						callMapGenerator();
 						dialog.dispose();
 					}
@@ -90,124 +94,157 @@ public class RogueExecutor {
 					dialog.revalidate();
 					dialog.repaint();
 				}
-		}
+			}
 		});
-        
-        dialog.setLocationRelativeTo(null);
-        dialog.setVisible(true);
+
+		dialog.setLocationRelativeTo(null);
+		dialog.setVisible(true);
 	}
-	
+
 	public void callMapGenerator(){
-		map = new Map(boundarySize);	
+		map = new Map(BoundarySize);
+		for(int i = 0; i < BoundarySize; i++){
+			for(int j = 0; j < BoundarySize; j++){
+				//System.out.println(map.getMapArray()[i][j].getRoomContent());
+			}
+		}	
 		initUI();
-		placeCharacter();
 		play();
 	}
-	
-	 private void play() {
-		 KeyListener listener = new KeyListener() {
-			 int previousX, previousY;
-				@Override
-				public void keyPressed(KeyEvent e) {
-					int keyCode = e.getKeyCode();
-					switch( keyCode ) { 
-			        case KeyEvent.VK_UP:
-			        	previousX = player.getX();
-			        	previousY = player.getY();
-			        	controller.moveNorth();			   
-			        	updateCellExploredStatus(player.getX(), player.getY());
-			        	//consoleLabel.setText(consoleTxt);
-			        	System.out.println(consoleTxt);
-			        	updateCurrentCell(player.getX(), player.getY(), previousX, previousY);		    
-			            break;
-			        case KeyEvent.VK_DOWN:
-			        	previousX = player.getX();
-			        	previousY = player.getY();
-			            controller.moveSouth();
-			            //consoleLabel.setText(consoleTxt);
-			            System.out.println(consoleTxt);
-			            updateCellExploredStatus(player.getX(), player.getY());
-			            //UpdateConsoleMessage(player.getX(), player.getY());
-			            updateCurrentCell(player.getX(), player.getY(), previousX, previousY);
-			            break;
-			            
-			        case KeyEvent.VK_LEFT:
-			        	previousX = player.getX();
-			        	previousY = player.getY();
-			            controller.moveWest();
-			            //consoleLabel.setText(consoleTxt);
-			            System.out.println(consoleTxt);
-			            updateCellExploredStatus(player.getX(), player.getY());
-			            //UpdateConsoleMessage(player.getX(), player.getY());
-			            updateCurrentCell(player.getX(), player.getY(), previousX, previousY);
-			            break;
-			            
-			        case KeyEvent.VK_RIGHT :
-			        	previousX = player.getX();
-			        	previousY = player.getY();
-			            controller.moveEast();
-			            //consoleLabel.setText(consoleTxt);
-			            System.out.println(consoleTxt);
-			            updateCellExploredStatus(player.getX(), player.getY());
-			            //UpdateConsoleMessage(player.getX(), player.getY());
-			            updateCurrentCell(player.getX(), player.getY(), previousX, previousY);
-			            break;
-			            
-			        case KeyEvent.VK_SPACE :
-			        	if(player.getX() == startX && player.getY() == startY){
-			        		System.exit(0);
-			        	}
-			            break;
-			     }
-					
-				}
 
-//				private void UpdateConsoleMessage(int PlayerX, int PlayerY) {
-//					if(map.getMapArray()[PlayerX][PlayerY].getRoomContent().equals("G"))
-//						consolePanel.add()
-//					
-//				}
+	private void play() {
 
-				private void updateCellExploredStatus(int x, int y) {
-					map.getMapArray()[x][y].setExplored(true);
-				}
-				
-				private void updateCurrentCell(int x, int y, int preX, int preY) {
-					cells[x][y].setBackground(Color.BLUE);
-					cells[preX][preY].setBackground(Color.WHITE);
-//					gamePanel.repaint();
-					if(map.getMapArray()[x][y].getRoomContent().equals("T")){
+		placeCharacter();
+		KeyListener listener = new KeyListener() {
+			int previousX, previousY;
+			@Override
+			public void keyPressed(KeyEvent e) {
+				int keyCode = e.getKeyCode();
+				switch( keyCode ) { 
+				case KeyEvent.VK_UP:
+					previousX = player.getX();
+					previousY = player.getY();
+					controller.moveNorth();			   
+					updateCellExploredStatus(player.getX(), player.getY());
+					updateCurrentCell(player.getX(), player.getY(), previousX, previousY);		    
+					break;
+				case KeyEvent.VK_DOWN:
+					previousX = player.getX();
+					previousY = player.getY();
+					controller.moveSouth();
+					updateCellExploredStatus(player.getX(), player.getY());
+					updateCurrentCell(player.getX(), player.getY(), previousX, previousY);
+					//gameFrame.repaint();
+					break;
+				case KeyEvent.VK_LEFT:
+					previousX = player.getX();
+					previousY = player.getY();
+					controller.moveWest();
+					updateCellExploredStatus(player.getX(), player.getY());
+					updateCurrentCell(player.getX(), player.getY(), previousX, previousY);
+					//gameFrame.repaint();
+					break;
+				case KeyEvent.VK_RIGHT :
+					previousX = player.getX();
+					previousY = player.getY();
+					controller.moveEast();
+					updateCellExploredStatus(player.getX(), player.getY());
+					updateCurrentCell(player.getX(), player.getY(), previousX, previousY);
+					//gameFrame.repaint();
+					break;
+				case KeyEvent.VK_SPACE :
+					if(player.getX() == startX && player.getY() == startY){
 						System.exit(0);
 					}
+					
+					if(map.mapArray[player.getX()][player.getY()].getRoomContent().equals("G")){
+						player.addScore(10);
+						map.mapArray[player.getX()][player.getY()].setRoomContent("0");
+						System.out.println(player.getScore());
+					} 
+					if(map.mapArray[player.getX()][player.getY()].getRoomContent().equals("S")){
+						player.lvlUp();
+						map.mapArray[player.getX()][player.getY()].setRoomContent("0");
+						System.out.println(player.getLevel());
+					}  
+					if(map.mapArray[player.getX()][player.getY()].getRoomContent().equals("E")){
+						if(player.getLevel() == enemy.getLevel()){
+							RogueExecutor.consoleTxt += "\nYOU WON .........";
+							System.out.println("won");
+							player.addScore (10 * enemy.getLevel());
+							map.mapArray[player.getX()][player.getY()].setRoomContent("0");
+						} else {
+							RogueExecutor.consoleTxt += "\nYOU LOST, DIED .........";
+							System.out.println("lose");
+							System.exit(0);
+						}					
 						
+						
+					} 
+					
+					break;
+				}
+
+			}
+
+			private void updateCellExploredStatus(int x, int y) {
+				map.getMapArray()[x][y].setExplored(true);
+			}
+
+			private void updateCurrentCell(int x, int y, int preX, int preY) {
+				cells[x][y].setBackground(Color.BLUE);
+				cells[preX][preY].setBackground(Color.WHITE);
+				
+				gameFrame.repaint();
+				if(map.getMapArray()[x][y].getRoomContent().equals("G")){
+					cells[preX][preY].setIcon(new ImageIcon(getClass().getResource("goldusama.jpg")));
+					consoleTxtLabel.setText(consoleTxt);
+					gameFrame.repaint();
 				}
 				
-				@Override
-				public void keyReleased(KeyEvent e) {
-					
+				if(map.getMapArray()[x][y].getRoomContent().equals("T")){
+					//cells[preX][preY].setIcon(new ImageIcon(getClass().getResource("goldusama.jpg")));
+					consoleTxtLabel.setText(consoleTxt);
+				 	gameFrame.repaint();
 				}
+				
+				if(map.getMapArray()[x][y].getRoomContent().equals("E")){
+					//cells[preX][preY].setIcon(new ImageIcon(getClass().getResource("goldusama.jpg")));
+					consoleTxtLabel.setText(consoleTxt);
+					gameFrame.repaint();
+				}
+				
+				if(map.getMapArray()[x][y].getRoomContent().equals("S")){
+					//cells[preX][preY].setIcon(new ImageIcon(getClass().getResource("goldusama.jpg")));
+					consoleTxtLabel.setText(consoleTxt);
+					gameFrame.repaint();
+				}
+			}
 
-				@Override
-				public void keyTyped(KeyEvent e) {
-					// TODO Auto-generated method stub
-					
-				}
+			@Override
+			public void keyReleased(KeyEvent e) {
+
+			}
+
+			@Override
+			public void keyTyped(KeyEvent e) {
+				// TODO Auto-generated method stub
+
+			}
 		};
-			
-		 gameFrame.addKeyListener(listener);
-}
+
+		gameFrame.addKeyListener(listener);
+	}
 
 	private void initUI() {
-		JLabel dummy = new JLabel();
-		dummy.setText("lalala");
-		
-		cells = new JLabel[map.getMapBoundary()][map.getMapBoundary()];
+		 consoleTxtLabel = new JLabel();
+		 cells = new JLabel[map.getMapBoundary()][map.getMapBoundary()];
 		 
 		 consolePanel = new JPanel();
 		 consolePanel.setBounds(map.getMapBoundary() * CELL_SIZE, 0, CONSOLE_PANEL_SIZE, map.getMapBoundary() * CELL_SIZE);
 		 consolePanel.setBackground(Color.GREEN);
-		 consolePanel.setLayout(new BorderLayout());
-		 consolePanel.add(dummy);
+		 consolePanel.setLayout(new FlowLayout());
+		 consolePanel.add(consoleTxtLabel,0);
 		 
 		 gamePanel = new JPanel();
 		 gamePanel.setBounds(0,0,map.getMapBoundary() * CELL_SIZE, map.getMapBoundary() * CELL_SIZE);
@@ -216,22 +253,21 @@ public class RogueExecutor {
 		 gameFrame = new JFrame();
 		 gameFrame.setTitle("Game");
 		 gameFrame.setSize(map.getMapBoundary() * CELL_SIZE  + CONSOLE_PANEL_SIZE, map.getMapBoundary() * CELL_SIZE);
-		 
-		 Border border = BorderFactory.createLineBorder(Color.GRAY, 5);
+		 gameFrame.setLayout(null);
+		 Border border = BorderFactory.createLineBorder(Color.GRAY, 2);
 		 
 		 for(int i = 0; i < map.getMapBoundary(); i ++){
 			 for(int j = 0; j < map.getMapBoundary(); j ++){
 				 cells[i][j] = new JLabel();
 				 cells[i][j].setSize(CELL_SIZE, CELL_SIZE);
-				 cells[i][j].setVisible(true);
+				 //cells[i][j].setVisible(true);
 				 cells[i][j].setOpaque(true);
 				 cells[i][j].setBackground(Color.BLACK);
 				 cells[i][j].setBorder(border);
 				 gamePanel.add(cells[i][j]);
 			 }
-		 }
+		 } 
 		 
-		 consolePanel.setVisible(true);
 		 gameFrame.add(gamePanel);
 		 gameFrame.add(consolePanel);
 		 gameFrame.setLocationRelativeTo(null);
@@ -240,74 +276,78 @@ public class RogueExecutor {
 	}
 
 
-public void placeCharacter(){
-	player =  new Player();
-	boolean charPlaced = false;
-	while(!charPlaced){
-		Random r1 = new Random();
-		Random r2 = new Random();
-		int x = r1.nextInt(boundarySize);
-		int y = r2.nextInt(boundarySize);
-		if(map.getMapArray()[x][y].getRoomContent().equals("0")){
-					player.setX(x);
-					player.setY(y);
+	public void placeCharacter(){
+		player =  new Player();
+		enemy = new Enemy();
+		boolean charPlaced = false;
+		while(!charPlaced){
+			Random r1 = new Random();
+			Random r2 = new Random();
+			int x = r1.nextInt(BoundarySize);
+			int y = r2.nextInt(BoundarySize);
+			if(map.getMapArray()[x][y].getRoomContent().equals("0")){
+				player.setX(x);
+				player.setY(y);
 
-					startX = x;
-					startY = y;
-					
-					//System.out.println("player coord: " + player.getX() + "," + player.getY());
-					charPlaced = true;
+				startX = x;
+				startY = y;
+
+				//System.out.println("player coord: " + player.getX() + "," + player.getY());
+				charPlaced = true;
+			}
+
+
 		}
 
-		
-	}
-	
 
-	
-	cells[player.getX()][player.getY()].setText("B");
-	gameFrame.repaint();
-	
-	controller = new RogueController(player, map.getMapBoundary());
+
+		cells[player.getX()][player.getY()].setText("B");
+		gameFrame.repaint();
+
+		cells[trapX][trapY].setText("T");
+		gameFrame.repaint();
+
+		controller = new RogueController(player, map.getMapBoundary());
 	}
-	
+
 }
 
 
 class Room {
-	
+
 	private boolean isExplored;
 	private String roomContent = ""; //T -> trap, E -> enemy, S -> sword, G -> gold, 0 -> empty
-	
+
 	public boolean getIsExplored(){
 		return isExplored;
 	}
-	
+
 	public void setExplored(boolean isExplored){
 		this.isExplored = isExplored;
 	}
-	
+
 	public void setRoomContent(String roomContent){
 		this.roomContent = roomContent;
 	}
-	
+
 	public String getRoomContent(){
 		return roomContent;
 	}
-	
-	
+
+
 }
 
 class Map {
-	
+
 	private int mapSize;
 	protected Room[][] mapArray;
-	
+
 	public Map(int mapSize){
 		this.mapSize = mapSize;
 		mapArray = new Room[mapSize][mapSize];
 		generateMap();
 	}
-	
+
 	public void generateMap(){
 		//System.out.println("hello I'm here!! ");
 		initRooms();
@@ -316,7 +356,7 @@ class Map {
 		placeGold();
 		placeSword();
 	}
-	
+
 	public void initRooms(){
 		//System.out.println("hello I'm here!! ");
 		for (int i = 0; i < mapSize; i++){
@@ -327,15 +367,15 @@ class Map {
 			}
 		}
 	}
-	
+
 	public Room[][] getMapArray(){
 		return mapArray;
 	}
-	
+
 	public int getMapBoundary(){
 		return mapSize;
 	}
-	
+
 	public ImageIcon getImage(Room room){
 		ImageIcon image;
 		if(room.getIsExplored()){
@@ -345,28 +385,30 @@ class Map {
 			image = new ImageIcon("question.jpg");
 			image.getImage();
 		}
-		
+
 		return image;
 	}
 	public void placeTraps(){ //assumes that the dungeon has "mapSize" amount of traps.
-		Random random = new Random();
 		int trapCount = 0;
 		while(trapCount < mapSize){
+			Random random = new Random();
 			int x = random.nextInt(mapSize);
 			int y = random.nextInt(mapSize);
 			if(mapArray[x][y].getRoomContent().equals("0")){ //if there is nothing in the room
-				mapArray[x][y].setRoomContent("T");
+				mapArray[x][y].setRoomContent("T");	
+				RogueExecutor.trapX = x;
+				RogueExecutor.trapY = y;
 			} else {
 				continue;
 			}
 			trapCount++;
 		}
 	}
-	
+
 	public void placeEnemies(){ //assumes that the dungeon has "mapSize" amount of enemies.
-		Random random = new Random();
 		int enemiesCount = 0;
 		while(enemiesCount < mapSize){
+			Random random = new Random();
 			int x = random.nextInt(mapSize);
 			int y = random.nextInt(mapSize);
 			if(mapArray[x][y].getRoomContent().equals("0")){ //if there is nothing in the room
@@ -377,11 +419,11 @@ class Map {
 			enemiesCount++;
 		}
 	}
-	
+
 	public void placeGold(){ //assumes that the dungeon has "mapSize" amount of traps.
-		Random random = new Random();
 		int goldCount = 0;
 		while(goldCount < mapSize){
+			Random random = new Random();
 			int x = random.nextInt(mapSize);
 			int y = random.nextInt(mapSize);
 			if(mapArray[x][y].getRoomContent().equals("0")){ //if there is nothing in the room
@@ -392,16 +434,16 @@ class Map {
 			goldCount++;
 		}
 	}
-	
+
 	public void placeSword(){ //places only one sword.
 		Random random = new Random();
-			int x = random.nextInt(mapSize);
-			int y = random.nextInt(mapSize);
-			if(mapArray[x][y].getRoomContent().equals("0")){ //if there is nothing in the room
-				mapArray[x][y].setRoomContent("S");
-			}
+		int x = random.nextInt(mapSize);
+		int y = random.nextInt(mapSize);
+		if(mapArray[x][y].getRoomContent().equals("0")){ //if there is nothing in the room
+			mapArray[x][y].setRoomContent("S");
+		}
 	}
-	
+
 	public void checkRoom(Player player, Enemy enemy){
 		int x = player.getX();
 		int y = player.getY();
@@ -435,17 +477,17 @@ class Map {
 			player.setScore (player.getScore() + 10);
 		}
 	}
-	
+
 }
 
 
 class Player {
-	
+
 	private int level;
 	private int score;
 	private int x;
 	private int y;
-	
+
 	public Player(){
 		level = 0;
 		score = 0;
@@ -466,9 +508,13 @@ class Player {
 	public void setScore(int score) {
 		this.score = score;
 	}
-	
+
 	public void addScore(int point) {
 		this.score += point;
+	}
+
+	public void lvlUp() {
+		this.level += 1;
 	}
 
 	public int getX() {
@@ -486,18 +532,17 @@ class Player {
 	public void setY(int y) {
 		this.y = y;
 	}
-	
+
 }
 
 class Enemy {
-	
+
 	private int level;
 	private int x;
 	private int y;
-	
-	public Enemy(int x, int y){
-		this.x = x;
-		this.y = y;
+
+	public Enemy(){
+		level = 1;
 	}
 
 	public int getLevel() {
@@ -523,6 +568,6 @@ class Enemy {
 	public void setY(int y) {
 		this.y = y;
 	}	
-	
+
 }
 
